d660b92f6b406601c18026b85d8ff7995ef6ff30
jwong1219 <jwong1219@gmail.com>
2017-09-21 01:30:35 -0700

    uploading progress with scripting 99.9% done, some minor bugs present, mainly the lives span is not updating for some reason, but all other mechanics work. Basic html with bootstrapping is done. Just need to work on css file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,60 +22,122 @@ function playDeflect() {
   deflect.play();
 }
 
+function removeTrooper(lives) {
+  switch (lives) {
+    case 6:
+      document.getElementById("trooper-6").style.visibility = "hidden";
+      break;
+    case 5:
+      document.getElementById("trooper-5").style.visibility = "hidden";
+      break;
+    case 4:
+      document.getElementById("trooper-4").style.visibility = "hidden";
+      break;
+    case 3:
+      document.getElementById("trooper-3").style.visibility = "hidden";
+      break;
+    case 2:
+      document.getElementById("trooper-2").style.visibility = "hidden";
+      break;
+    case 1:
+      document.getElementById("trooper-1").style.visibility = "hidden";
 
+  }
+}
 
+function resetTroopers() {
+  var x = document.getElementsByClassName(".trooper");
+  for (var i=0; i < x.length; i++) {
+    x[i].style.visibility = "visible";
+  }
+}
 
-//console.log(alphArray);
-//console.log("the length of the words array is: " + lenList);
-//console.log(blasts);
 
 var score = 0;
 
 function game() {
-  //make all images with the class of trooper visible
   var lives = 6;
   var guessed = [];
+  document.querySelector("#guesses").innerHTML = guessed;
   var word = wordsList[(Math.floor(Math.random() * lenList))];
-  //console.log("the randomly selected word is " + word);
+  console.log("the randomly selected word is " + word);
+  document.querySelector("#alert").innerHTML = ("May the Force be with you, Rogues");
+  resetTroopers();
 
-  var displayWord = [];
-  for ( var i = 0; i<word.length; i++) {
+  var modifiedWord = "";
+  for (var i=0; i<word.length; i++) {
     if (word[i] !== " ") {
-      displayWord[i] = "_";
+      modifiedWord += (word[i] + " ");
     }
     else {
-      displayWord[i] = " ";
+      modifiedWord += (" ");
     }
   }
-  console.log("the displayWord is " + displayWord);
-
-  //rewrite the html span with displayWord
+  console.log({modifiedWord});
+  var displayWord = "";
+  for (var i=0; i<modifiedWord.length; i++) {
+    if (modifiedWord[i] !== " ") {
+      displayWord += ("_");
+    }
+    else {
+      displayWord += (" ");
+    }
+  }
+  document.querySelector("#displayCode").innerHTML = displayWord; //rewrite the html span with displayWord
+  console.log({displayWord});
+  
 
   document.onkeyup = function(event) {
     var letter = event.key;
+    console.log({letter});
     if(guessed.indexOf(letter) === -1 && alphArray.indexOf(letter) !== -1) {
       playBlasts();
-      //playBlasts();
-      var loseLife = true;
-      for (var i=0; i<word.length; i++) {
-        if (letter === word[i]) {
-          displayWord[i] = letter;
-          loseLife = false;
+      var loseLife = true; //starts off true, and will be changed to false if the letter is found in the word
+      // for (var i=0; i<modifiedWord.length; i++) {
+      //   if (letter === modifiedWord[i]) {
+      //     displayWord.charAt(i) = letter; //for some reason, this is not happening.
+      //     loseLife = false;
+      //     console.log("the position is " + i);
+      //     console.log("the letter at this position is " + modifiedWord[i]);
+      //   }
+      // }
+      var upDateWord = "";
+      for (var i=0; i<modifiedWord.length; i++) {
+        if (letter === modifiedWord[i]) {
+        upDateWord += letter; //for some reason, this is not happening.
+        loseLife = false;
+        console.log("the position is " + i);
+        console.log("the letter at this position is " + modifiedWord[i]);
+        }
+        else {
+          upDateWord += displayWord[i];
         }
       }
-      guessed.push(letter);
+      displayWord = upDateWord; 
+
+      guessed.push(letter); //adds the guessed letter to the array
+      document.querySelector("#displayCode").innerHTML = displayWord;
       console.log(displayWord);
-      if (loseLife) {
-        console.log("letters you have guessed: " + guessed);
+
+      document.querySelector("#guesses").innerHTML = guessed;
+      console.log("letters you have guessed: " + guessed);
+
+      if (loseLife) { //if the player guessed incorrectly, losesLife is still true
+        
         setTimeout(playDeflect, 500);
         setTimeout(playScream, 1500);
         //playScream();
-        //make trooper-(lives) hidden;
+        setTimeout(removeTrooper, 1700, lives);//make trooper-(lives) hidden;
         lives--;
         console.log("Lives remaining: " + lives);
+        //document.querySelector("#displayLives") = String(lives);
+        
         if (lives === 0) {
-          alert("You have failed to stop Darth Vader, but you may live to fight another day");
-          game();
+          document.querySelector("#alert").innerHTML = ("You have failed to stop Darth Vader, but you may live to fight another day. Press any key to try again");
+          //alert("You have failed to stop Darth Vader, but you may live to fight another day");
+          document.onkeyup = function(event){
+            game();
+          }
         }
       }
       else 
@@ -83,11 +145,30 @@ function game() {
       if (displayWord.indexOf("_") === -1 ) { //This means there are no more hidden letters, and the player has won
         score ++;
         nooo.play();
-        alert("You have stopped Darth Vader!! Good work Rogues!");
-        game();
+        document.querySelector("#alert").innerHTML = ("You have stopped Darth Vader!! Good work Rogues! Press any key for your next mission!");
+        //alert("You have stopped Darth Vader!! Good work Rogues!");
+        document.onkeyup = function(event){
+          game();
+        }
       }
     }
   }
 }
+
+// function updateDisWord(word) {
+//   var upDateWord = "";
+//   for (var i=0; i<modifiedWord.length; i++) {
+//     if (letter === modifiedWord[i]) {
+//       upDateWord += letter; //for some reason, this is not happening.
+//       loseLife = false;
+//       console.log("the position is " + i);
+//       console.log("the letter at this position is " + modifiedWord[i]);
+//     }
+//     else {
+//       upDateWord += modifiedWord[i];
+//     }
+//   }   
+//   word =  upDateWord;
+// }
 
 game();
